66d4177a57aa10266f2d7dc9662564e6635e6b5e
YannickVanHecke <yannickvanhecke@MacBook-Pro-van-Yannick-2.local>
2017-11-29 13:20:25 +0100

    fix(list/detail): fixing error with the project.

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 
+import {HeroService} from './hero.service';
+import {MessageService} from './message.service';
 
 @NgModule({
   declarations: [
--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/observable';
 import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
